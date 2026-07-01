dbe8a0e8af1ec2121243d0ecfce7af8ae91ae05c
Jolie Ehab <jolie.ehabgsg@gmail.com>
2026-07-02 01:12:45 +0300

    Add ticket view with reusable widgets

--- lib/base/res/styles/app_styles.dart
+++ lib/base/res/styles/app_styles.dart
@@ -8,14 +8,31 @@ class AppStyles {
   static Color primaryColor = primary;
   static Color textColor = const Color(0xFF3b3b3b);
   static Color bgColor = const Color(0xFFEEEDF2);
+  static Color ticketBlue = const Color(0xFF526799);
+  static Color ticketOrange = const Color(0xFFF37B67);
+
+  static TextStyle textStyle= TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      color: textColor);
+
 
   static TextStyle headLineStyle1= TextStyle(
       fontSize: 26,
       fontWeight: FontWeight.bold,
       color: textColor);
 
+  static TextStyle headLineStyle2= TextStyle(
+      fontSize: 21,
+      fontWeight: FontWeight.bold,
+      color: textColor);
+
   static TextStyle headLineStyle3= const TextStyle(
       fontSize: 17,
       fontWeight: FontWeight.w500,);
+
+  static TextStyle headLineStyle4= const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,);
 
 }
